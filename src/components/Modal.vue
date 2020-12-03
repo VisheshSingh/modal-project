a0ef3cdb7eef5d5eb8b2062f82a2609cfd6cc49d
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-02 21:30:11 -0600

    Multiple Components

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+.backdrop {
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+</style>
